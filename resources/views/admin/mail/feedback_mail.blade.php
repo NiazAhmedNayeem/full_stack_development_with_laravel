75ef33eba2c3f1a118efda53aff69f319f7a0afb
NiazAhmedNayeem <niazahmed.net@gmail.com>
2023-03-27 02:09:42 +0600

    Input code on feedback_mail file

---
+++ resources/views/admin/mail/feedback_mail.blade.php
@@ -0,0 +1,31 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <style>
+        .div-one{
+            height: auto;
+            width: 60%;
+            margin: 0 auto;
+            background-color: #2a3042;
+            color: white;
+            text-align: center;
+            padding: 40px;
+        }
+    </style>
+</head>
+<body>
+<div class="div-one">
+    <h4>Name: {{ $value['name'] }}</h4>
+    <p>Email: {{ $value['email'] }}</p>
+    <p>Subject: {{ $value['subject'] }}</p>
+    <p>Message: {{ $value['message'] }}</p>
+    <hr/>
+    <p>Thank you..</p>
+</div>
+</body>
+</html>
